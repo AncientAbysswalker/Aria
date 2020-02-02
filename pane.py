43e1965e5bdda9c8ed729b4b906b0cac43625182
Ancient Abysswalker <ancientabysswalker@gmail.com>
2020-02-02 09:13:07 -0800

    >Add condition for if no PDFs in list >Remove more unwanted code (printers)

--- pane.py
+++ pane.py
@@ -70,8 +70,6 @@ class PaneMain(wx.Panel):
                 event: A button event
         """
 
-        ListPrinters()
-
         # Open an explorer dialog to select images to import
         with wx.FileDialog(None, "Open",
                            wildcard="pdf files (*.pdf)|*.pdf",
@@ -99,24 +97,27 @@ class PaneMain(wx.Panel):
                 event: A button event
         """
 
-        # Assemble PDF
-        pdf_merger = PyPDF2.PdfFileMerger(strict=False)
-        for document in self.ls_paths:
-            pdf_merger.append(document)
+        # Check if the list is populated
+        if self.ls_paths:
+            print(356)
+            # Assemble PDF
+            pdf_merger = PyPDF2.PdfFileMerger(strict=False)
+            for document in self.ls_paths:
+                pdf_merger.append(document)
 
-        with wx.FileDialog(None, "Save",
-                           wildcard="pdf files (*.pdf)|*.pdf",
-                           style=wx.FD_SAVE) as file_dialog:
+            with wx.FileDialog(None, "Save",
+                               wildcard="pdf files (*.pdf)|*.pdf",
+                               style=wx.FD_SAVE) as file_dialog:
 
-            # Check if the user changed their mind about importing
-            if file_dialog.ShowModal() == wx.ID_CANCEL:
-                return
+                # Check if the user changed their mind about importing
+                if file_dialog.ShowModal() == wx.ID_CANCEL:
+                    return
 
-            # Save merged file to selected location
-            pdf_merger.write(file_dialog.GetPath())
+                # Save merged file to selected location
+                pdf_merger.write(file_dialog.GetPath())
 
-        # Close merging gracefully
-        pdf_merger.close()
+            # Close merging gracefully
+            pdf_merger.close()
 
     def evt_clear(self, event):
         """Clears the widget listing files to merge
@@ -131,84 +132,3 @@ class PaneMain(wx.Panel):
 
         # Reset widget
         self.wgt_added_docs.SetValue("")
-
-
-# def PrinterTest(s):
-#     out("win32ui printing test")
-#     th = 100
-#     x = 50
-#     y = 50
-#
-#     lines = s.split("\n")
-#
-#     hDC = win32ui.CreateDC()
-#     hDC.CreatePrinterDC("Brother HL-5370DW series")    #! change printer name
-#
-#     hDC.StartDoc("Python Printer Test")
-#     hDC.StartPage()
-#     for line in lines:
-#         hDC.TextOut(x, y, line)
-#         y += th
-#
-#     hDC.EndPage()
-#     hDC.EndDoc()
-#
-# def MSWinPrintTest(s):
-#     out("MSWinPrint test")
-#     doc = MSWinPrint.document() #! prints to default printer if nothing specified
-#     doc.begin_document()
-#
-#     size = doc.getsize()
-#     out("document size: %d, %d" % size)
-#
-#     out("setting font")
-#     doc.setfont("Consolas", 6)
-#
-#     x  = 72*6   # 18 # .25"
-#     y  = 18
-#     th = 6
-#
-#     out("printing lines")
-#     lines = s.split("\n")
-#
-#     for line in lines:
-#         doc.text((x, y), line)
-#         y += th
-#
-#     out("printing image")
-#     img = Image.open("c:\projects\pics\NorthCapeCodMap.bmp")
-#     scale = 0.20
-#     neww = int(img.size[0] * scale)
-#     newh = int(img.size[1] * scale)
-#     doc.image((18,18), img, (neww, newh))           #! change image names
-#
-#     out("printing another image")
-#     img = Image.open("C:\projects\pics\Windham-20110503-00005.jpg")
-#     scale = 0.10
-#     neww = int(img.size[0] * scale)
-#     newh = int(img.size[1] * scale)
-#     doc.image((18, 72*6), img, (neww, newh))
-#
-#     doc.rectangle((0, 0, 72*7.9, 72*10.25))
-#     out("finishing up")
-#     doc.end_document()
-#     out("done")
-
-
-def ListPrinters():
-    default = MSWinPrint.listprinters()
-    print(default)
-    print("-"*20)
-    for p in MSWinPrint.prdict:
-        print(p)
-    print("-"*20)
-#
-#
-# s = gu.LoadFile(__file__)
-#
-# PrinterTest(s)
-# #z MSWinPrintTest(s)
-#
-# #z ListPrinters()
-
-###
